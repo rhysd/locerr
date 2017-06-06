05c0a7d75370a3878036f611d89d23a2f6bd51bf
rhysd <lin90162@yahoo.co.jp>
2017-06-06 13:49:54 +0900

    update examples and screenshot

--- example_test.go
+++ example_test.go
@@ -10,12 +10,14 @@ func TestExample(t *testing.T) {
 	// At first you should gain entire source as *Source instance.
 
 	code :=
-		`package main
+		`function foo(x: bool): int {
+  return (if x then 42 else 21)
+}
 
-func main() {
-	foo := 42
-
-	foo := true
+function main() {
+  foo(true,
+      42,
+      "test")
 }`
 	src := NewDummySource(code)
 
@@ -24,14 +26,14 @@ func main() {
 	// Let's say to find an error at some range in the source.
 
 	start := Pos{
-		Offset: 41,
+		Offset: 88,
 		Line:   6,
-		Column: 2,
+		Column: 7,
 		File:   src,
 	}
 	end := Pos{
-		Offset: 52,
-		Line:   6,
+		Offset: 116,
+		Line:   9,
 		Column: 12,
 		File:   src,
 	}
@@ -39,41 +41,44 @@ func main() {
 	// NewError or other factory functions make a new error instance with the range. Error instance implements
 	// error interface so it can be handled like other error types.
 
-	err := ErrorIn(start, end, "Found duplicate symbol 'foo'")
+	err := ErrorIn(start, end, "Calling 'foo' with wrong number of argument")
 
 	// Assume that you find additional information (location of variable and its type). Then you can add some
 	// notes to the error. Notes can be added by wrapping errors like pkg/errors library.
 
 	prev := Pos{
-		Offset: 26,
-		Line:   4,
-		Column: 1,
+		Offset: 9,
+		Line:   1,
+		Column: 10,
 		File:   src,
 	}
 
-	err = err.NoteAt(prev, "Defined here at first")
-	err = err.NoteAt(prev, "Previously defined as int")
+	err = err.NoteAt(prev, "Defined with 1 parameter")
+	err = err.NoteAt(prev, "'foo' was defined as 'bool -> int'")
 
 	// Finally you can see the result!
 
 	// Get the error message as string. Note that this is only for non-Windows OS.
 	fmt.Println(err)
 	// Output:
-	// Error: Found duplicate symbol 'foo' (at <dummy>:6:1)
-	//     Note: Defined here at first (at <dummy>:4:1)
-	//     Note: Previously defined as int (at <dummy>:4:1)
+	// Error: Calling 'foo' with wrong number of argument (at <dummy>:6:7)
+	//   Note: Defined with 1 parameter (at <dummy>:1:10)
+	//   Note: 'foo' was defined as 'bool -> int' (at <dummy>:1:10)
 	//
-	// >       foo := true
+	// >   foo(true,
+	// >       42,
+	// >       "test")
 	//
 
 	// Directly writes the error message into given file.
 	// This supports Windows. Useful to output from stdout or stderr.
 	err.PrintToFile(os.Stdout)
 	// Output:
-	// Error: Found duplicate symbol 'foo' (at <dummy>:6:1)
-	//     Note: Defined here at first (at <dummy>:4:1)
-	//     Note: Previously defined as int (at <dummy>:4:1)
+	// Error: Calling 'foo' with wrong number of argument (at <dummy>:6:7)
+	//   Note: Defined with 1 parameter (at <dummy>:1:10)
+	//   Note: 'foo' was defined as 'bool -> int' (at <dummy>:1:10)
 	//
-	// >       foo := true
-	//
+	// >   foo(true,
+	// >       42,
+	// >       "test")
 }
